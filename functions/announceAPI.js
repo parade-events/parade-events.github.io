dc57bb2bf5c3cd5ea47df1a50139b3184880b1f2
Josh Spicer <hello@joshspicer.com>
2018-08-23 17:26:13 -0400

    before going out to eat

--- functions/announceAPI.js
+++ functions/announceAPI.js
@@ -6,35 +6,76 @@ xhttp.onreadystatechange = function() {
 
     console.log(res);
 
-    // Hide our shame when it all goes wrong.
+    // If it breaks we must have nothing to say.....
     if (!res) {
-      return;
+      output = "No announcements!";
     }
 
-    let output = '';
+    let output = "";
 
     res.forEach(function(item) {
-      var str = ["<p>",'[i]<b>', item.title, "</b><br>", item.description, '</p>'];
+      var str = [
+        "<span>",
+        '<img class="infoIcon" src="./assets/info.png/>',
+        '"<b><a class="announceTitle" href="',
+        item.link,
+        '">',
+        item.title,
+        "</a></b><br><div class='announceDescription'>",
+        convertTimestamp(item.date),
+        "- ",
+        item.description,
+        "</div></span>"
+      ];
+
       output += str.join(" ");
+      output += '<br>';
     });
 
-
-
-
-
     document.getElementById("announce").innerHTML = output;
-
-      // songStatus +
-      // "<span id='spotifyGreen'>" +
-      // songName +
-      // "</span>" +
-      // filler1 +
-      // "<span id='spotifyGreen'>" +
-      // artistName +
-      // "</span>" +
-      // filler2
-      // + spotifyStar;
   }
 };
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
+
+function convertTimestamp(timestamp) {
+  // Credits: https://gist.github.com/kmaida/6045266
+  var d = new Date(timestamp * 1000), // Convert the passed timestamp to milliseconds
+    yyyy = d.getFullYear(),
+    mm = ("0" + (d.getMonth() + 1)).slice(-2), // Months are zero based. Add leading 0.
+    dd = ("0" + d.getDate()).slice(-2), // Add leading 0.
+    hh = d.getHours(),
+    h = hh,
+    min = ("0" + d.getMinutes()).slice(-2), // Add leading 0.
+    ampm = "AM",
+    time;
+
+  if (hh > 12) {
+    h = hh - 12;
+    ampm = "PM";
+  } else if (hh === 12) {
+    h = 12;
+    ampm = "PM";
+  } else if (hh == 0) {
+    h = 12;
+  }
+
+  time = months[mm - 1] + ". " + dd;
+  return time;
+}
+
 xhttp.open("GET", "https://parade.events/announce/news.json", true);
 xhttp.send();
